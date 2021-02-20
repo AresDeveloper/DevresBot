1888fc8c84e2f6a51a02d567ba909db63e357d71
Ares-coder819 <76408386+Ares-coder819@users.noreply.github.com>
2021-02-20 17:22:44 +0100

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,6 +1,6 @@
 module.exports = (client) => {
-    const channelId = '811355120918462467' // welcome channel
-    const targetChannelId = '811317438876614747' // rules and info
+    const channelId = ' ' // welcome channel
+    const targetChannelId = ' ' // rules and info
   
     client.on('guildMemberAdd', (member) => {
       const message = `Please welcome <@${
